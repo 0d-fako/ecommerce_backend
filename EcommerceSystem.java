f69aec1780fec322a3344656932989942a168b39
0d-fako <od.fakorede@gmail.com>
2025-01-17 12:44:49 +0100

    Ecommerce Backend

--- EcommerceSystem.java
+++ EcommerceSystem.java
@@ -2,8 +2,20 @@ import java.util.*;
 
 
 public class EcommerceSystem {
-    private Scanner input = new Scanner(System.in);
-    private 
+    private Scanner input;
+    private List<Product> products;
+    private List<Customer> customers;
+    private List<Order> orders;
+    private int nextOrderId = 1;
+
+    public EcommerceSystem() {
+        input = new Scanner(System.in);
+        products = new ArrayList<>();
+        customers = new ArrayList<>();
+        orders = new ArrayList<>();
+        
+    }
+
 
 
 }
